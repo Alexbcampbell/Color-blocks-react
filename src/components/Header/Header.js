04df32cdd8d07d7796c15ba73e11d18562fd68ec
Alex Campbell <alexbe.campbell@gmail.com>
2020-12-28 16:49:08 -0600

    Imported redux and react dependencies, added sagas/reducers, mapStoreToProps and updated index.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Header.css';
+import mapStoreToProps from '../../Redux/mapStoreToProps';
 
 class Header extends Component {
+  onClickRed = (event) => {};
+
   render() {
+    // const colorButton = this.props.store.colorReducer.map(item, index) => {
+    //   return()
+    // }
+
     return (
       <header className="appTopper">
         <h1 className="appHdg">{this.props.hdg}</h1>
         <div className="appTopper-toolbar">
-          <div>Add Colours</div>
+          <div>
+            <button>Add Red</button>
+            <button>Add Blue</button>
+            <button>Add Green</button>
+            <button>Add Yellow</button>
+          </div>
           <div>Layout</div>
         </div>
       </header>
@@ -15,4 +28,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default connect(mapStoreToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
+import rootReducer from '../src/Redux/Reducers/_root_reducer';
+import rootSaga from '../src/Redux/Sagas/_root_saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewareList =
+  process.env.NODE_ENV === 'development'
+    ? [sagaMiddleware, logger]
+    : [sagaMiddleware];
+
+const store = createStore(
+  // tells the saga middleware to use the rootReducer
+  // rootSaga contains all of our other reducers
+  rootReducer,
+  // adds all middleware to our project including saga and logger
+  applyMiddleware(...middlewareList)
+);
+
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
